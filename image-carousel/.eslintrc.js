f5f680338053aa3b6424e5e3cc5e6c15e0e70a56
Drac Zihper <draczihper@gmail.com>
2024-07-20 21:28:27 +0300

    Modify eslint config

--- image-carousel/.eslintrc.js
+++ image-carousel/.eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     root: true,
-    extends: ["airbnb-base", "prettier"],
-    rules: {},
+    extends: ['airbnb-base', 'prettier'],
+    rules: {
+        'no-undef': 'warn',
+    },
 };
